14f8c1574364209a1b19ab83098c2804e8ed6b91
Hanin <haeun5419@gmail.com>
2021-09-12 21:48:35 +0900

    css 다듬지 X - 일단 출력되는 것 확인했음

--- myvenv/mysite/liftapi/views.py
+++ myvenv/mysite/liftapi/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from .models import Lift
 
 def lift_view(request):
     lifts = Lift.objects.all()
-    return render(request,'index.html',{"lift" : lifts})
+
+# return render(request,'index.html',{"lift" : lifts})
+    return render(request,'index.html',{'lifts' : lifts})
 
