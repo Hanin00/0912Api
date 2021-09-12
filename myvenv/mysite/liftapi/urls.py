35bdcc62f8876e3c31b02990f1dcf6967bf58a40
Hanin <haeun5419@gmail.com>
2021-09-12 22:56:19 +0900

    Lift viewset 적용 / post / get 가능 + total 과 auto 고려해서 수정 필요

--- myvenv/mysite/liftapi/urls.py
+++ myvenv/mysite/liftapi/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from liftapi import views
+from . import views
 from django.conf.urls import url,include
 from django.contrib import admin
+from .views import LiftViewSet
+from rest_framework import routers
 
 
 app_name = 'lift'
 
 urlpatterns = [
-    path('', views.lift_view, name = 'index'),
+    path('list/', views.lift_view, name = 'index'),
+    path('total/', views.lift_list),
+    path('liftdetail/<int:pk>/', views.lift_detail),
 ]
